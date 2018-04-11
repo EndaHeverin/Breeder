74c4cc0d921e069f442b4cde009fbe3b41791108
EndaHeverin <endaheverin@hotmail.com>
2018-04-11 23:51:12 +0100

    Add files via upload

--- users.js
+++ users.js
@@ -1,25 +1,94 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
-require('./util');
+var express = require('express');
+var router = express.Router();
+var User = require('../models/users');
+var jwt = require('jsonwebtoken');
 
-var usersSchema = new Schema({
-    user_name: {type: String},
-    password: String,
-    fb_id: { type: String, default: null },
-    access_token: String
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
-/*
- * Hashes the password for storage in the DB
- */
-usersSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
 
-// Compares passwords to determine if the user is who they say they are
-usersSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-};
+router.post('/register', function(req, res, next){
+    var username = req.body.user_name;
+    var password = req.body.password;
+    // Check if account already exists
+    User.findOne({ 'user_name' :  username }, function(err, user)
+    {
+        if (err)
+            res.send(err);
+        // check to see if theres already a user with that email
+        if (user) {
+            res.status(401).json({
+                "status": "info",
+                "body": "Username already taken"
+            });
+        } else {
+            // If there is no user with that username create the user
+            var newUser = new User();
 
-module.exports = mongoose.model('User', usersSchema);
+            // set the user's local credentials
+            newUser.user_name = username;
+            newUser.password = newUser.generateHash(password);
+            newUser.access_token = createJwt({user_name:username});
+            newUser.save(function(err, user) {
+                if (err)
+                    throw err;
+                res.cookie('Authorization', 'Bearer ' + user.access_token);
+                res.json({'success' : 'account created'});
+
+            });
+        }
+    });
+});
+
+router.post('/login', function (req, res, next) {
+    var username = req.body.user_name;
+    var password = req.body.password;
+    User.findOne({'user_name': username}, function (err, user) {
+        // if there are any errors, return the error
+        if (err)
+            res.send(err);
+        // If user account found then check the password
+        if (user) {
+            // Compare passwords
+            if (user.validPassword(password)) {
+                // Success : Assign new access token for the session
+                user.access_token = createJwt({user_name: username});
+                user.save();
+                res.cookie('Authorization', 'Bearer ' + user.access_token);
+                res.json({'success': 'loggedIn'});
+            }
+            else {
+                res.status(401).send({
+                    "status": "error",
+                    "body": "Email or password does not match"
+                });
+            }
+        }
+        else {
+            res.status(401).send({
+                "status": "error",
+                "body": "Username not found"
+            });
+        }
+    });
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
+router.get('/login', function(req, res, next) {
+res.render('login');
+});
+
+
+function createJwt(profile) {
+    return jwt.sign(profile, 'CSIsTheWorst', {
+        expiresIn: '10d'
+    });
+}
+
+module.exports = router;
